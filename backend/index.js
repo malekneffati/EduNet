4771543341ec47055e3c693476e3e8f6643d44c3
Malek Neffati <malekneffati912@gmail.com>
2025-12-06 16:59:23 +0100

    test render9

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import "dotenv/config";
 // Firebase Admin
 import admin from "firebase-admin";
 const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, "\n")
+  process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\\\n/g, "\n")
 );
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
